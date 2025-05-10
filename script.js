675683f9eb11e1db5d0e5f59312d8fc33a632c13
MuriloSouza955 <souza.murilo@gmail.com>
2025-05-10 18:07:38 -0300

    Obtendo o comprimento de uma string

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /*
-Nesta aula, vamos aprender como exibir o conteúdo de uma string em maiúsculo e minúsculo no JavaScript. Vamos usar o método toUpperCase para transformar o texto em maiúsculo e o método toLowerCase para transformar em minúsculo. Esses métodos não modificam o conteúdo da variável, apenas exibem o texto de acordo com a formatação desejada. Essas são opções úteis para manipular strings no JavaScript.
+Obtendo o comprimento de uma string
+Nesta aula, vamos descobrir como obter o comprimento de uma string em JavaScript usando a propriedade .length para retornar a quantidade de caracteres de uma string, incluindo espaços. A propriedade é útil para validar senhas com um número mínimo de caracteres. Também veremos como verificar o comprimento de um número, convertendo-o em string para utilizar a propriedade .length. Alternativamente, também é possível usar o método toString() para obter o mesmo resultado.
 
 */
 
@@ -7,8 +8,11 @@ let message = "Estou estudando os fundamentos do Javascript."
 
 console.log(message)
 
-//Exibir o textyo em maiusculo
-console.log(message.toUpperCase())
+//como obter o comprimento de uma string
+console.log(message.length)
 
-//Exibir o texto em minúsculo
-console.log(message.toLowerCase())
+// obter a quantidade de dígitos de um número
+let number = 123456789
+// console.log(number.length)
+console.log(number.toString().length) // ou
+console.log(String(number).length)
