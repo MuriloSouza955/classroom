be74706768277ad19ef9613c61fda876329d4a9e
MuriloSouza955 <souza.murilo@gmail.com>
2025-05-10 19:07:28 -0300

    Substituindo e fatiando uma string

--- script.js
+++ script.js
@@ -6,13 +6,29 @@ Nesta aula, vamos descobrir como obter o comprimento de uma string em JavaScript
 
 let message = "Estou estudando os fundamentos do Javascript."
 
-console.log(message)
+// console.log(message)
 
 //como obter o comprimento de uma string
-console.log(message.length)
+// console.log(message.length)
 
 // obter a quantidade de dígitos de um número
-let number = 123456789
+// let number = 123456789
 // console.log(number.length)
-console.log(number.toString().length) // ou
-console.log(String(number).length)
+// console.log(number.toString().length) // ou
+// console.log(String(number).length)
+
+// console.log(message.replace("os fundamentos do Javascript", "HTM    L")) // substitui a palavra Javascript por HTML
+
+//Extraindo uma parte da string
+console.log(message)
+
+console.log(message.slice(0, 5)) // extrai a parte de 5 caracteres
+console.log(message.slice(6, 30)) // extrai a parte de 6 até 30 caracteres
+console.log(message.slice(-11)) // extrai a última parte de 11 caracteres
+
+//removendo espaços em branco no inicio e fim da string
+let message2 = "    Estou estudando os fundamentos do Javascript.    "
+console.log(message2)
+console.log(message2.length)
+console.log(message2.trim()) // remove espaços em branco no inicio e fim da string
+console.log(message2.trim().length)
