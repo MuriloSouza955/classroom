6f21566a681a4a056dfdccfc60c5ce88dc1e1700
MuriloSouza955 <souza.murilo@gmail.com>
2025-05-12 16:49:17 -0300

    3-1 Aprendendo sobre arrays

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 /*
-Encontrando um conteúdo no texto
-Nesta aula, vamos aprender a verificar a existência e a posição de um conteúdo em uma string no JavaScript. Utilizaremos o método indexOf() para encontrar a posição de uma palavra e o método includes para verificar se uma palavra ou conteúdo existe na string. É importante lembrar que o JavaScript é "case sensitive", ou seja, faz diferença entre maiúsculas e minúsculas. Podemos combinar esses métodos com toLowerCase e toUpperCase para facilitar as comparações. Esses métodos são úteis para diversas aplicações.
+Aprendendo sobre arrays
+Nesta aula, vamos aprender o conceito de array, que é uma coleção ordenada de valores, onde cada item possui uma posição específica chamada de índice. Vamos fazer uma analogia com uma caixa de correio com compartimentos numerados, representando os índices do array. Vamos aprender também que os índices começam do zero e vão até um número determinado, sendo o total de índices igual ao número final menos um.
 
 */
 
-let message = "Estou estudando os fundamentos do JavaScript"
-
-// Obtém a posição da palavra.
-console.log(message.indexOf("estudando"))
-
-//Quando nao encontra a palavra, retorna -1.
-console.log(message.indexOf("javascript"))
-
-//uma saida, para "driblar" o case sentivive, é usar o método toLowerCase ou toUpperCase.
-console.log(message.toLowerCase().indexOf("javascript"))
-console.log(message.toUpperCase().indexOf("JAVASCRIPT"))
-
-// Verifica se a palavra existe na string.
-console.log(message.includes("JavaScript"))
-console.log(message.includes("HTML"))
