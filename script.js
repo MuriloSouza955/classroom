d70b9359f974d462e6519626f0b51a7be411d9d5
MuriloSouza955 <souza.murilo@gmail.com>
2025-05-12 19:38:58 -0300

    3-07 Quais elementos um array aceita?

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 /*
-Usando o índice
-Nesta aula, vamos aprender como obter a posição de um item em um array, utilizando o método indexOf. Também iremos aprender a deletar um item específico pelo índice, utilizando o método splice. O método splice permite remover elementos a partir de uma posição específica no array.
+Quais elementos o array aceita?
+Nesta aula, vamos ver que um array pode conter elementos de diferentes tipos de dados, como texto, números, booleanos, funções e objetos.
+
 */
 
-let fruits = ['banana', 'apple', 'orange', 'grape'];
-console.log(fruits);
+let myArray = [
+    "Texto",
+    123,
+    true,
+    function umaFuncao() {
+        console.log("Eu sou uma função")
+    },
+    { name: "Murilo", age: 25 },
+    [1, 2, 3, 4, 5],
+    new Date(),
+    null,
+    undefined,
+]
+console.log(myArray)
 
-// Obtendo a posição de um item em um array
-let position = fruits.indexOf('orange');
-console.log(position);
-
-// fruits.splice(1, 2); // Removendo itens a partir do índice 1, removendo 2 itens
-// O array fruits agora é ['banana', 'grape']
-
-fruits.splice(position, 1); // Removendo o item 'orange' do array
-
-console.log(fruits);
+myArray[3]() // Chama a função que está dentro do array
