4545ba3a047f5368a2d706d300ee983a69f72b90
MuriloSouza955 <souza.murilo@gmail.com>
2025-05-13 15:59:31 -0300

    For of

--- script_aula4.js
+++ script_aula4.js
@@ -1,22 +1,25 @@
 /*
-For in
-Nesta aula, vamos aprender sobre a estrutura de repetição for in, que permite iterar sobre as propriedades de um objeto. Também foi mostrado como utilizar o for in com arrays, percorrendo seus índices ou elementos. O for in é uma ferramenta útil para percorrer objetos e arrays de forma eficiente.
+For of
+Nesta aula, iremos aprender sobre a estrutura de repetição for of, que itera sobre os valores de um objeto iterável. Diferente do for in, o for off itera sobre os valores, não sobre as propriedades. É importante que o objeto seja iterável, como um array.
 */
 
-let person = {
-    name: "Murilo",
-    age: 30,
-    city: "Santa Catarina",
-    state: "SC",
-    country: "Brasil"
+let students = ["Murilo", "Maria", "Pedro", "José"];
+
+for (let student of students) {
+    console.log(student);
 }
 
-for (let property in person) {
-    console.log(property + ": " + person[property]); // Exibe o nome da propriedade e seu valor
+for (let student in students) {
+    console.log(student);
 }
-console.log("======================");
 
-let students = ["Murilo", "Lucas", "João", "Maria", "Ana"];
-for (let index in students) {
-    console.log(students[index]);
+let users = [
+    { name: "Murilo", age: 20 },
+    { name: "Maria", age: 19 },
+    { name: "Pedro", age: 21 },
+    { name: "José", age: 22 }
+]
+
+for (let user of users) {
+    console.log(user.name);
 }
