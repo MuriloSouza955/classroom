63e3ca7ee9f8a94a1857baeb88660f7c3645244d
MuriloSouza955 <souza.murilo@gmail.com>
2025-05-13 16:04:53 -0300

    Break

--- script_aula4.js
+++ script_aula4.js
@@ -1,25 +1,28 @@
 /*
-For of
-Nesta aula, iremos aprender sobre a estrutura de repetição for of, que itera sobre os valores de um objeto iterável. Diferente do for in, o for off itera sobre os valores, não sobre as propriedades. É importante que o objeto seja iterável, como um array.
+Break
+Nesta aula, vamos aprender um pouco mais sobre o uso da palavra reservada break para encerrar a execução de repetições, como no caso do switch case e em estruturas de repetição como o for. No exemplo do switch case, o break é utilizado para encerrar a execução de um caso e evitar a continuação para os próximos casos. Já no for, o break é usado para interromper a repetição em um determinado momento. O break é útil para controlar o fluxo de execução do código.
 */
 
-let students = ["Murilo", "Maria", "Pedro", "José"];
+let option = 3
 
-for (let student of students) {
-    console.log(student);
+switch (option) {
+    case 1:
+        console.log('Opção 1')
+        break
+    case 2:
+        console.log('Opção 2')
+        break
+    case 3:
+        console.log('Opção 3')
+        break
+    default:
+        console.log('Opção inválida')
 }
 
-for (let student in students) {
-    console.log(student);
-}
-
-let users = [
-    { name: "Murilo", age: 20 },
-    { name: "Maria", age: 19 },
-    { name: "Pedro", age: 21 },
-    { name: "José", age: 22 }
-]
-
-for (let user of users) {
-    console.log(user.name);
+// utilizando o break em um loop for
+for (let i = 0; i < 5; i++) {
+    if (i === 3) {
+        break
+    }
+    console.log(i)
 }
