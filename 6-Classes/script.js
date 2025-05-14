eb96ae9e6572acfad51ec33e99564129f56663fb
MuriloSouza955 <souza.murilo@gmail.com>
2025-05-14 19:37:56 -0300

    Sobrescrevendo métodos

--- 6-Classes/script.js
+++ 6-Classes/script.js
@@ -1,6 +1,6 @@
-/*  AULA 06 - Clases em JavaScript
-Como aplicar herança com classes?
-Nesta aula, vamos aprender como aplicar herança com classes em programação. A herança permite reutilizar propriedades e métodos de classes superiores.
+/*  AULA 07 - Clases em JavaScript
+Sobrescrevendo métodos
+Nessa aula vamos aprender como sobrescrever métodos.
 */
 
 class Animal {
@@ -10,15 +10,36 @@ class Animal {
   makeNoise() {
     console.log("Som genérico do animal");
   }
+  run() {
+    console.log(`${this.name} está correndo`);
+  }
 }
 
-class Dog extends Animal {}
-class Cat extends Animal {}
+class Dog extends Animal {
+  makeNoise() {
+    console.log("Au au!");
+  }
+    run() {
+        console.log(`${this.name} está correndo e latindo`);
+    }
+}
+class Cat extends Animal {
+  makeNoise() {
+    console.log("Meow ... Meow");
+  }
+  cacandoRatos() {
+    console.log("Cacando ratos");
+  }
+
+}
 
 const dog = new Dog("Fido");
 console.log(dog.name);
 dog.makeNoise();
+dog.run();
 
 const cat = new Cat("Felix");
 console.log(cat.name);
 cat.makeNoise();
+cat.run();
+cat.cacandoRatos();
