1a24600fb03f64a2cdcfbebf5d578c46fd4921e2
MuriloSouza955 <souza.murilo@gmail.com>
2025-05-14 19:46:31 -0300

    Criando um objeto prototype

--- 6-Classes/script.js
+++ 6-Classes/script.js
@@ -1,45 +1,23 @@
-/*  AULA 07 - Clases em JavaScript
-Sobrescrevendo métodos
-Nessa aula vamos aprender como sobrescrever métodos.
+/*  AULA 08 - Clases em JavaScript
+Aprendendo sobre herança e cadeia de protótipos
+Nesta aula, vamos aprender o conceito de prototype chain no JavaScript, que se baseia em herança e cadeia de protótipos. Vamos entender como objetos herdam propriedades e métodos de seus protótipos ascendentes, até chegar a um protótipo com valor nulo, finalizando a cadeia.
 */
 
-class Animal {
-  constructor(name) {
-    this.name = name;
-  }
-  makeNoise() {
-    console.log("Som genérico do animal");
-  }
-  run() {
-    console.log(`${this.name} está correndo`);
-  }
-}
+/*  AULA 09 - Prototypes em JavaScript
+Criando um objeto prototype
+Nesta aula, exploraremos a herança de objetos em JavaScript, demonstrando na prática o uso do prototype. Vamos aprender como objetos como endereço e arrays possuem protótipos que podem ser explorados para entender a cadeia de herança. Compreender essa estrutura de herança em JavaScript é fundamental para aprofundar seus conhecimentos na linguagem.
+*/
 
-class Dog extends Animal {
-  makeNoise() {
-    console.log("Au au!");
-  }
-    run() {
-        console.log(`${this.name} está correndo e latindo`);
-    }
-}
-class Cat extends Animal {
-  makeNoise() {
-    console.log("Meow ... Meow");
-  }
-  cacandoRatos() {
-    console.log("Cacando ratos");
-  }
+const address = {
+  city: "São Paulo",
+  country: "Brasil",
+};
 
-}
+console.log(address);
 
-const dog = new Dog("Fido");
-console.log(dog.name);
-dog.makeNoise();
-dog.run();
+const users = ["Murilo", "João", "Pedro", "Maria"];
 
-const cat = new Cat("Felix");
-console.log(cat.name);
-cat.makeNoise();
-cat.run();
-cat.cacandoRatos();
+console.log(users);
+
+const user_name = "Murilo Souza";
+console.log(user_name.__proto__);
