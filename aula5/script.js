92fded8443ced11e0a47f695d0bdbbc8033e1e00
MuriloSouza955 <souza.murilo@gmail.com>
2025-05-13 22:29:17 -0300

    5-Obtendo e exibindo data e hora

---
+++ aula5/script.js
@@ -0,0 +1,11 @@
+/*
+Obtendo a data e a hora
+Nesta aula, vamos aprender como obter datas e horas no JavaScript.
+*/
+//Exibindo a data e hora atual
+console.log(new Date());
+//Exibindo a data e hora de marco 0 (1970)
+console.log(new Date(0));
+
+//Exibindo a data e hora de marco 1970, até o momento em segundos
+console.log(new Date().getTime());
