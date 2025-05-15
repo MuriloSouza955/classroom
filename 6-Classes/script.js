e15fef37a422ce1370e64a238422d0c3fd82924d
MuriloSouza955 <souza.murilo@gmail.com>
2025-05-14 21:22:46 -0300

    Como utilizar classes para criar erros customizados

--- 6-Classes/script.js
+++ 6-Classes/script.js
@@ -1,26 +1,21 @@
-/*  AULA 10 - Clases em JavaScript
-Como utilizar classes pra lidar com exceções
-Nesta aula, aprenderemos a utilizar classes para lidar com exceções específicas em JavaScript. Vamos ver como identificar e tratar diferentes tipos de erros, como TypeError e RangeError. Também foi abordado o uso do método throwNew para gerar exceções personalizadas. Vamos mostrar a importância de tratar exceções de forma específica e amigável para o usuário, assim como a possibilidade de encadear diferentes tipos de exceções.
+/*  AULA 11 - Clases em JavaScript
+Como utilizar classes para criar erros customizados
+Nesta aula, aprenderemos a criar erros personalizados usando classes em JavaScript. A personalização de erros oferece flexibilidade na aplicação.
 */
 
-let obj = [17, 42, 13, 99, 100, 110, 120];
-let index = 300
+class MyCustomError {
+  constructor(message) {
+    this.message = "Classe de erro customizada: " + message;
+  }
+}
 
 try {
-  if (!obj.includes(17)) {
-    throw new Error("Não foi possível encontrar o valor 17 no objeto");
-  }
-  if (index > 99) {
-    throw new RangeError("O valor do índice é maior que 99");
-  }
-} catch (e) {
-  if (e instanceof TypeError) {
-    console.log(e);
-  }
-  if (e instanceof RangeError) {
-    console.log(e.message);
+  throw new Error("Erro padrão lançado");
+  throw new MyCustomError("Erro personalizado lançado");
+} catch (error) {
+  if (error instanceof MyCustomError) {
+    console.log(error.message);
   } else {
-    console.log("Erro desconhecido");
+    console.log("Não foi possivel executar");
   }
-//   console.log(e);
 }
